d8aa8c87a670affeec0ee765bd63e60a2e3116eb
David Richardson <drichardson42@gatech.edu>
2020-02-13 15:27:34 -0500

    Add check_mat function
    Introduce check_mat to verify the contents of the .mat files being
    input.
    Introduce the file_check flag to load_rx_data to skip check_mat if the
    user wants to save some runtime and verify the .mat files themselves.

--- src/lfdata.py
+++ src/lfdata.py
@@ -10,15 +10,17 @@ from datetime import datetime
 from scipy.io import loadmat
 
 
-def load_rx_data(mat_file, variables=None):
+def load_rx_data(mat_file, variables=None, file_check=True):
     """ Properly format an LF AWESOME receiver's output mat file
 
     Parameters
     ----------
     mat_file : string
         File path to a specific .mat file
-    variables : list of strings (optional)
+    variables : list of strings, optional
         List of variables to be extracted from the .mat file
+    file_check : boolean, optional
+        Flag to check whether the input mat_file and variables are valid
 
     Returns
     -------
@@ -29,6 +31,14 @@ def load_rx_data(mat_file, variables=None):
     --------
     LFData : Data management class
     """
+    if file_check:
+        validity = check_mat(mat_file, variables)
+        if not validity["mat_file"]:
+            raise ValueError("Input .mat file is not a valid data file.")
+        elif not validity["variables"]:
+            raise ValueError(
+                "One or more variables are not contained in the .mat file."
+            )
     data = loadmat(mat_file, mat_dtype=True, variable_names=variables)
     for key in data:
         if key in [
@@ -89,3 +99,63 @@ def load_rx_data(mat_file, variables=None):
         )
 
     return data
+
+
+def check_mat(mat_file, variables=None):
+    """ Check if a .mat file is a valid LF data mat file
+
+    Parameters
+    ----------
+    mat_file : string
+        Path to .mat file
+    variables : list of str, optional
+        List of variables of interest
+
+    Returns
+    -------
+    Dict
+        Dictionary of booleans for the validity of the mat_file and variables
+
+    """
+    data = loadmat(mat_file)
+    expected_keys = [
+        "latitude",
+        "longitude",
+        "altitude",
+        "Fs",
+        "gps_quality",
+        "adc_channel_number",
+        "adc_sn",
+        "adc_type",
+        "antenna_bearings",
+        "antenna_description",
+        "cal_factor",
+        "computer_sn",
+        "gps_sn",
+        "hardware_description",
+        "is_broadband",
+        "station_description",
+        "station_name",
+        "VERSION",
+        "is_amp",
+        "is_msk",
+        "Fc",
+        "call_sign",
+        "filter_taps",
+        "data",
+        "start_second",
+        "start_minute",
+        "start_hour",
+        "start_day",
+        "start_month",
+        "start_year",
+    ]
+    validity = {}
+    validity["mat_file"] = True
+    validity["variables"] = True
+    if set(data.keys()) != set(expected_keys):
+        validity["mat_file"] = False
+    if variables is not None:
+        if not all(elem in expected_keys for elem in variables):
+            validity["variables"] = False
+    return validity
