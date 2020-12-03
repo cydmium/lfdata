89d873d38e19c9e1c5643096c9351bb82e2bd49c
David Richardson <drichardson42@gatech.edu>
2020-12-03 16:54:31 -0500

    Update Docs version number

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = "2020, David Richardson"
 author = "David Richardson"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.3"
+release = "0.1.5"
 
 
 # -- General configuration ---------------------------------------------------
